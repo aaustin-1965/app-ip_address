1abde11c44b0116dae1431452e69058fb765402e
aaustin-1965 <andrew.austin.2020.mark@gmail.com>
2019-08-12 18:46:44 +0000

    lab 2 part 1

--- main.js
+++ main.js
@@ -95,7 +95,6 @@ function getIpv4MappedIpv6Address(ipv4) {
   return ipv6Address;
 }
 
-
 /*
   This section is used to test function and log any errors.
   We will make several positive and negative tests.
@@ -134,7 +133,6 @@ function main() {
     }
   }
 }
-
 
 
 /*
