0bd5d763c1164ad8b1fab60d43937975435794a6
aaustin-1965 <andrew.austin.2020.mark@gmail.com>
2020-12-30 23:15:59 +0000

    This is a working lab 4

--- views/tasks/ManualFirstIpAddress/controller.js
+++ views/tasks/ManualFirstIpAddress/controller.js
@@ -1,29 +1,11 @@
-// Define variable taskData, later assigned to this.variables in an exported anonymous function.
-// IAP sets _this.variable.incoming_ to the task's runtime arguments,
-// and passes the copy as an argument to TaskController's first parameter.
-// As a best practice, define the manual task's parameters as properties of the incoming object
-// with null values for easy reference when modifying your code.
-var taskData = {
-  incoming: {
-    cidrStr: null,
-  }
-};
-
-// Define variable taskFinishState, later assignd to this.finish_state in an exported
-// anonymous function.
-// IAP passes the exported string an argument to
-// TaskController's second parameter.
-// This variable's purpose has changed over the life of IAP.
-// It isn't necessarily used in modern IAP releases, but it remains to
-// avoid backwards compatibility problems.
-var taskFinishState = null;
-
-
 /**
- * AngularJS controller function.
+ * @function TaskController
+ * @description An AngularJS Material $mdDialog controller function.
  * @param {object} variables - An object carrying the IAP manual task's arguments.
  * @param {object} variables.incoming - Tasks's arguments.
- * @param {string} finish_state - No longer used.
+ * @param {string} variables.incoming.cidrStr - An IPv4 subnet expressed in CIDR notation.
+ * @param {string} finish_state - No longer used. This parameter's purpose has changed over
+ *                                the life of IAP. It remains to avoid backwards compatibility problems.
  * @param {object} $scope - [AngularJS built-in $scope object]{@link https://code.angularjs.org/1.5.3/docs/guide/scope}
  *                          used to transfer data from the
  *                          controller to the view and visa-versa.
@@ -36,59 +18,52 @@ var taskFinishState = null;
  *                             critical information or require them to make decisions.
  */
 function TaskController(variables, finish_state, $scope, $http, $mdDialog) {
-  let cidrRegex = /^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))[/]([0-9]|[1-2][0-9]|3[0-2])$/;
-  $scope.cidrRegex = new RegExp(cidrRegex);
-  $scope.cidrStr = variables.incoming.cidrStr;
+
   let returnedData = {
     ipAddressData: null
   };
   let finishState = 'error';
 
+  // cidrRegex is a regular expression to match IPv4 CIDR addresses.
+  // It will be used to validate user data.
+  // We define it here and make it available to our AngularJS
+  // application by assigning it to the $scope object's cidrRegex
+  // property.
+  const cidrRegex = /^(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.){3}(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))[/]([0-9]|[1-2][0-9]|3[0-2])$/;
+  $scope.cidrRegex = new RegExp(cidrRegex);
+
+  // If an optional argument was passed for parameter cidrStr,
+  // assign $scope.cidrStr to parameter variables.incoming.cidrStr.
+  // This will pre-populate the dialog's CIDR input, which is bound
+  // to Angular variable cidrStr, to any optional passed argument.
+  $scope.cidrStr = ( variables.incoming.cidrStr ? variables.incoming.cidrStr : '' );
+
+  /**
+   * Set $scope's cancelDialog property to a function that calls $mdDialog's
+   * .cancel() method. We will associate this method to a button users can
+   * press if they wish to cancel working the manual task.
+   */
   $scope.cancelDialog = function() {
     $mdDialog.cancel();
   }
 
+  /**
+   * Set $scope's submit property to a function that calls $mdDialog's
+   * .hide() method. We will associate this method to a button users can
+   * press when they wish to complete working the manual task.
+   */
   $scope.submit = function() {
-    $http({
-      method: 'GET',
-      url: '/ip-address/firstIpAddress/' + encodeURIComponent($scope.cidrStr),
-    }).then(
-      function successCallback(response) {
-        returnedData.ipAddressData = response.data || {};
-        finishState = 'success';
-        $mdDialog.hide({
-          finish_state: finishState,
-          variables: returnedData,
-        });
-      },
-      function errorCallback(response) {
-        returnedData.ipAddressData = response.data || {};
-        finishState = 'error';
-        $mdDialog.hide({
-          finish_state: finishState,
-          variables: returnedData,
-        });
-      },
-    );
+    returnedData.ipAddressData = {
+      youEntered: $scope.cidrStr
+    };
+    finishState = 'success';
+    $mdDialog.hide({
+      finish_state: finishState,
+      variables: returnedData,
+    });
   };
-
-  function setFocusToTextBox(focusId) {
-    let textbox = document.getElementById(focusId);
-    textbox.focus();
-    textbox.scrollIntoView();
-  }
-
-  const focusToElementWithId = 'cidrInput';
-  console.log('What is document.getElementById(' + focusToElementWithId + '): ' + document.getElementById(focusToElementWithId));
-  if (document.getElementById(focusToElementWithId)) {
-    console.log('Calling setFocusToTextBox()');
-    setFocusToTextBox(focusToElementWithId);
-  };
-
 }
 
 module.exports = function() {
-  this.variables = taskData;
-  this.finish_state = taskFinishState;
   this.TaskController = TaskController;
 };
